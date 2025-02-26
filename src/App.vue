4feb2f91455435b4f93e741b135e892725ae4e95
hossein seyedi <hosseincybercoin@gmail.com>
2025-02-26 12:05:15 +0330

    fix

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 import { onMounted } from 'vue'
 
 const prefix = 'ton://'
-const amount = 0.01 *1e9 
+const amount = 0.01 * 1e9 
 const address=  'UQAsV3nfqjnd-yk9rMlhR1NSsoCjqYbdgJTFAutt-PRC1Gf-'
 const text = '5184890136'
 
@@ -10,7 +10,8 @@ const redirectUri = `${window.location.origin}/return_after_receive`
 
     const openLik = ()=>{
         const url = `${prefix}transfer/${address}?amount=${amount}&text=${text}`
-        window.open(url, '_blank')
+
+        window.open(url)
     }
 
 onMounted(() => {
